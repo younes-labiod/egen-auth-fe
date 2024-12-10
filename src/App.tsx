ab91e330fae4d6feba5f204bc781474ab420ddae
younes-labiod <youneslabiod@gmail.com>
2024-12-10 22:47:19 +0400

    setup privaate route for application page

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,27 @@ import SignUpPage from "./components/SignUpPage/SignUpPage";
 import SignInPage from "./components/SignInPage/SignInPage";
 import ApplicationPage from "./components/ApplicationPage/ApplicationPage";
 
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({ children }: { children: JSX.Element }) => {
+  const token = localStorage.getItem("token");
+  return token ? children : <Navigate to="/signin" />;
+};
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<SignUpPage />} />
         <Route path="/signin" element={<SignInPage />} />
-        <Route path="/application" element={<ApplicationPage />} />
+
+        <Route
+          path="/application"
+          element={
+            <PrivateRoute>
+              <ApplicationPage />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </Router>
   );
